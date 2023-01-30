f08cf582a358b80df8e74bc71492b1ec2927c5c4
chonzing <105171881+RafkoZieba@users.noreply.github.com>
2023-01-30 19:48:34 +0100

    test sieć prywatna

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, W   orld!");
+Console.WriteLine("Hello, W   o   rld!");
+Console.WriteLine("Hello, W   o   rld!");
+Console.WriteLine("Hello, W   o   rld!");
+Console.WriteLine("Hello, W   o   rld!");
+Console.WriteLine("Hello, W   o   rld!");
+Console.WriteLine("Hello, W   o   rld!");
